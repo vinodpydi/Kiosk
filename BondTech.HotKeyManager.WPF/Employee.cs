ec4d0222de795f4613fe9cb28ac85c0e0c4e2741
503129107 <vinod.pydi@ge.com>
2019-06-04 11:46:27 +0530

    add the unit test changes

---
+++ BondTech.HotKeyManager.WPF/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BondTech.HotKeyManagement.WPF
+{
+   public class Employee
+    {
+        public string GetName(string firstName, string lastName)
+        {
+            return string.Concat(firstName, " ", lastName);
+        }  
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BondTech.HotKeyManagement.WPF;
+
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange  
+            Employee objEmployee = new Employee();
+            String firstName = "Vinod";
+            String lastName = "Pydi";
+            String expected = "Vinod";
+            String actual;
+            //Act  
+            actual = objEmployee.GetName(firstName, lastName);
+            //Assert  
+            Assert.AreEqual(expected, actual);  
+        }
+    }
+}
