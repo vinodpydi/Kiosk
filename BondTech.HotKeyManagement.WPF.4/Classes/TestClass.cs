74ca53e181934afb3e6642cf25226026b60e7444
503129107 <vinod.pydi@ge.com>
2019-07-18 10:34:38 +0530

    Add test class

---
+++ BondTech.HotKeyManagement.WPF.4/Classes/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BondTech.HotKeyManagement.WPF._4.Classes
+{
+   public class TestClass
+    {
+
+    }
+}
